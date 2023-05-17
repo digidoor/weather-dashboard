af1cfd51a2b791af56b42795cc3e5d2488f4f4e6
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-17 15:21:42 -0700

    extracting geo stuff almost working

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 apiKey = "e3f7cf22dd3edac262ac80be651ffa17";
-var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
-const apiGeo = 'https://api.openweathermap.org/geo/1.0/direct?q='
-const apiGeo2 = "&appid="
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=";
+const apiGeo = 'https://api.openweathermap.org/geo/1.0/direct?q=';
 var userFormEl = document.getElementById("userForm");
 var outputDiv = document.getElementById("outputDiv");
 
@@ -9,11 +8,15 @@ async function submitHandler()
 {
 	event.preventDefault();
 	outputDiv.textContent = "Form submmitted. Check the console.";
-	var geoPromise = await fetch(apiGeo + 'Sacramento' + apiGeo2 + apiKey);
+	var geoPromise = await fetch(apiGeo + 'Sacramento' + "&appid=" + apiKey);
 	console.log(geoPromise);
 	var geoData = await geoPromise.json();
 	console.log(geoData);
-
+	const { lon, lat } = geoData;
+	console.log( lon, lat);
+	var weatherPromise = await fetch(apiUrl + lat + "&lon=" + lon + "&appid=" + apiKey);
+	var weatherData = await weatherPromise.json();
 }
+
 
 userFormEl.addEventListener('submit', submitHandler);
