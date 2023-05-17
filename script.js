99d29fc71357f0016e7740948e145465dbd4e65d
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-17 15:15:28 -0700

    getting geo data, updated html, added css

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 apiKey = "e3f7cf22dd3edac262ac80be651ffa17";
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+const apiGeo = 'https://api.openweathermap.org/geo/1.0/direct?q='
+const apiGeo2 = "&appid="
+var userFormEl = document.getElementById("userForm");
+var outputDiv = document.getElementById("outputDiv");
+
+async function submitHandler()
+{
+	event.preventDefault();
+	outputDiv.textContent = "Form submmitted. Check the console.";
+	var geoPromise = await fetch(apiGeo + 'Sacramento' + apiGeo2 + apiKey);
+	console.log(geoPromise);
+	var geoData = await geoPromise.json();
+	console.log(geoData);
+
+}
+
+userFormEl.addEventListener('submit', submitHandler);
