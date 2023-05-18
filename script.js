09be9d7959d3101c4cdbd62db0da567f8ea1e123
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-17 20:27:27 -0700

    setup object for saving persistent history

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=";
 const apiGeo = 'https://api.openweathermap.org/geo/1.0/direct?q=';
 var userFormEl = document.getElementById("userForm");
 var outputDiv = document.getElementById("outputDiv");
+var cityInputEl = document.getElementById("userText");
 
 async function submitHandler()
 {
 	event.preventDefault();
-	let city = "Sacramento";
-	outputDiv.textContent = "Form submmitted. Check the console.";
+	let city = cityInputEl.value.trim();
+	outputDiv.textContent = "Thanks.";
 	var geoPromise = await fetch(`${apiGeo}${city}&appid=${apiKey}`);
 	var geoData = await geoPromise.json();
 	const { lon, lat } = geoData[0];
@@ -22,13 +23,9 @@ async function submitHandler()
 
 function displayWeather(data, city)
 {
-	let title = `${data.city.name} ${dateObj}`;
-	let desc = data.list[i].weather[0].description;
-	let temp = `Temperature: ${data.list[i].main.temp}F`;
-	let humid = `Humidity: ${data.list[i].main.humidity}%`;
-	let wind = `Wind Speed: ${data.list[i].wind.speed} km/h`;
+	var city = {};
+	city.name = data.city.name;
 	console.log(data.list[1].weather[0].description);
-	let i = 0;
 	for(let i=7; i<40; i=i+8)
 	{
 		let dateObj = dayjs.unix( data.list[i].dt ).format('MMMM D, YYYY h:mm A');
@@ -37,6 +34,17 @@ function displayWeather(data, city)
 		renderWeather( `Temperature: ${data.list[i].main.temp}F`, "p", outputDiv );
 		renderWeather( `Humidity: ${data.list[i].main.humidity}%`, "p", outputDiv );
 		renderWeather( `Wind Speed: ${data.list[i].wind.speed} km/h`, "p", outputDiv );
+		let title = `${data.city.name} ${dateObj}`;
+		let desc = data.list[i].weather[0].description;
+		let temp = `Temperature: ${data.list[i].main.temp}F`;
+		let humid = `Humidity: ${data.list[i].main.humidity}%`;
+		let wind = `Wind Speed: ${data.list[i].wind.speed} km/h`;
+		console.log("7/8: ", Math.floor(7/8));
+		city[`title${Math.floor(i/8)+1}`] = title;
+		city[`desc${Math.floor(i/8)+1}`] = desc;
+		city[`temp${Math.floor(i/8)+1}`] = temp;
+		city[`humid${Math.floor(i/8)+1}`] = humid;
+		city[`wind${Math.floor(i/8)+1}`] = wind;
 	}
 }
 
